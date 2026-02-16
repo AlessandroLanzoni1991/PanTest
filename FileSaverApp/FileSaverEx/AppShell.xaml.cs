d2eee0772fbad40946c449c4077e8f66a35985ae
AlessandroLanzoni1991 <104826665+AlessandroLanzoni1991@users.noreply.github.com>
2026-02-16 11:46:16 +0100

    Commit

---
+++ FileSaverApp/FileSaverEx/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FileSaverEx
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FileSaverApp/FileSaverEx/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Maui.Storage;
+using System.Text;
+
+namespace FileSaverEx
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object? sender, EventArgs e)
+        {
+            try
+            {
+                var text = "This is a text";
+                var fileStream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+
+                var result = await FileSaver.Default.SaveAsync("test.txt", fileStream, CancellationToken.None);
+            
+                if(!result.IsSuccessful)
+                    throw result.Exception;
+            }
+            catch(Exception ex)
+            {
+                var message = ex.Message;
+            }
+        }
+    }
+}
